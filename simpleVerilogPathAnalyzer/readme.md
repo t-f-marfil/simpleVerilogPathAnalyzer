fadf183485f3f9de4e5d90978fec65285a6f88ba
marfil <2000110205t.fukami@gmail.com>
2022-05-28 13:23:53 +0900

    is submodule now.

--- simpleVerilogPathAnalyzer/readme.md
+++
@@ -1,6 +0,0 @@
-# simple verilog path analyzer
-
-A simple analyzer of Verilog/System Verilog code.
-Intended to give a hint to find a critical path in the source.
-
-This branch is not for direct use.
--- simpleVerilogPathAnalyzer/svstats.py
+++
@@ -1,8 +0,0 @@
-from .svlex import lexer
-from .svyacc import parser
-# from .svast import Source
-# from .svutils import indent
-
-def parseSrc(data:str):
-    src = parser.parse(data, lexer=lexer, tracking=True)
-    return src
